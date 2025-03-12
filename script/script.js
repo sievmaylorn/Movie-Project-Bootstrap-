b8ea6cacc31de3a125886b349fb3d50cbd441024
lornsievmay <lornsievmay04@gamil.com>
2025-03-12 20:31:52 +0700

    second commit

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 new Typed('.auto-type', 
     {
-        strings:['ភាគយន្តខ្មែរ','ភាគយន្តបរទេស','ភាគយន្តផ្សេងៗ'],
+        strings:['រឿងខ្មែរ','រឿងបរទេស','រឿងផ្សេងៗ'],
         loop: true,
         typeSpeed: 120,
         backSpeed: 120,
@@ -18,7 +18,7 @@ window.onscroll = function(){
     if(scrollvalue > 120)
     {
         header.style.padding = "16px 0";
-        header.style.backgroundColor = "pink";
+        header.style.backgroundColor = "gray";
         header.style.transition = "0.3s ease-in";
     } else
     {
@@ -27,3 +27,116 @@ window.onscroll = function(){
         header.style.transition = "0.3s ease-out";
     }
 }
+
+// ----------Scary movie
+var movieAarr = [
+    {title:"ចញ្ជៀងខ្មោច",year:"២០១៨",duration:120,rate:4.3,category:"scary",img:"./assets/img/movie1.jpg"},
+    {title:"ឃើញក្បាលខ្ញំុទេ",year:"២០២២",duration:90,rate:3.9,category:"scary",img:"./assets/img/movie2.jpg"},
+    {title:"បណ្តាសារនាងរាត្រី",year:"២០១៩",duration:200,rate:5.8,category:"scary",img:"./assets/img/movie3.jpg"},
+    {title:"ខ្មោចពោះធំ",year:"២០២៤",duration:185,rate:2.2,category:"scary",img:"./assets/img/movie4.jpg"},
+    {title:"គ្រាប់ពេជ្រ",year:"២០១៤",duration:185,rate:2.2,category:"scary",img:"./assets/img/movie5.jpg"},
+]
+
+var scarymovie = document.getElementById('scarymovie');
+scarymovie.innerHTML = "";
+movieAarr.forEach((e)=>{
+    scarymovie.innerHTML += 
+    `                                <div class="col-12 col-md-6 col-lg-3">
+                                        <div class="px-2">
+                                            <div style="height: 450px;" class="bg-blue-100 overflow-hidden hover-image rounded-2 ">
+                                                <img src="${e.img}" 
+                                                alt="" class="w-100 h-100 object-fit-cover">
+                                            </div>
+
+                                            <div class="d-flex justify-content-between mt-3 mb-2">
+                                                <h3 class="text-light font-kh-bayon">${e.title}</h3>
+                                                <h3 class="text-blue-400 fw-bold">${e.year}</h3>
+                                            </div>
+                                            <div class="d-flex justify-content-between mt-3">
+                                                <p class="border-danger border-2 px-2 text-light fw-bold m-0 text-blue-400">HD</p>
+                                                <div class="pe-4 pe-md-2 pe-lg-0 d-flex align-items-center">
+                                                    <i class="bi bi-alarm me-2 fw-bolder text-light"></i>
+                                                    <p class="text-light m-0 fw-bolder">${e.duration}</p>
+                                                    &ensp;
+                                                    <i class="bi bi-star-fill me-2 fw-bolder text-blue-400 fw-bold"></i>
+                                                    <p class="text-light m-0 fw-bolder">${e.rate}</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                     </div> `
+})
+
+// -----------------Funny
+var movieAarr1 = [
+    {title:"ស្រឡាញ់បងល្អពេក",year:"២០១៨",duration:120,rate:4.3,category:"funny",img:"./assets/img/funny1.jpg"},
+    {title:"អាភ្លេច",year:"២០២២",duration:90,rate:3.9,category:"funny",img:"./assets/img/funny2.jpg"},
+    {title:"លោកប៉ាមហាកូរ",year:"២០១៩",duration:200,rate:5.8,category:"funny",img:"./assets/img/funny3.jpg"},
+    {title:"ប្រុសលក់ខ្លួន",year:"២០២៤",duration:185,rate:2.2,category:"funny",img:"./assets/img/funny4.jpg"},
+]
+
+var funnymovie = document.getElementById('funnymovie');
+funnymovie.innerHTML = " ";
+movieAarr1.forEach((e)=>{
+    funnymovie.innerHTML += 
+    `                                <div class="col-12 col-md-6 col-lg-3">
+                                        <div class="px-2">
+                                            <div style="height: 450px;" class="bg-blue-100 overflow-hidden hover-image rounded-2 ">
+                                                <img src="${e.img}" 
+                                                alt="" class="w-100 h-100 object-fit-cover">
+                                            </div>
+
+                                            <div class="d-flex justify-content-between mt-3 mb-2">
+                                                <h3 class="text-light font-kh-bayon">${e.title}</h3>
+                                                <h3 class="text-blue-400 fw-bold">${e.year}</h3>
+                                            </div>
+                                            <div class="d-flex justify-content-between mt-3">
+                                                <p class="border-danger border-2 px-2 text-light fw-bold m-0 text-blue-400">HD</p>
+                                                <div class="pe-4 pe-md-2 pe-lg-0 d-flex align-items-center">
+                                                    <i class="bi bi-alarm me-2 fw-bolder text-light"></i>
+                                                    <p class="text-light m-0 fw-bolder">${e.duration}</p>
+                                                    &ensp;
+                                                    <i class="bi bi-star-fill me-2 fw-bolder text-blue-400 fw-bold"></i>
+                                                    <p class="text-light m-0 fw-bolder">${e.rate}</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                     </div> `
+})
+
+// ------------------------History 
+var movieAarr2 = [
+    {title:"ស្រឡាញ់បងល្អពេក",year:"២០១៨",duration:120,rate:4.3,category:"history",img:"./assets/img/history1.jpg"},
+    {title:"អាភ្លេច",year:"២០២២",duration:90,rate:3.9,category:"history",img:"./assets/img/history2.jpg"},
+    {title:"លោកប៉ាមហាកូរ",year:"២០១៩",duration:200,rate:5.8,category:"history",img:"./assets/img/history3.jpg"},
+    {title:"ប្រុសលក់ខ្លួន",year:"២០២៤",duration:185,rate:2.2,category:"history",img:"./assets/img/history4.jpg"},
+]
+
+var historymovie = document.getElementById('historymovie');
+historymovie.innerHTML = " ";
+movieAarr2.forEach((e)=>{
+    historymovie.innerHTML += 
+         `                                <div class="col-12 col-md-6 col-lg-3">
+                                        <div class="px-2">
+                                            <div style="height: 450px;" class="bg-blue-100 overflow-hidden hover-image rounded-2 ">
+                                                <img src="${e.img}" 
+                                                alt="" class="w-100 h-100 object-fit-cover">
+                                            </div>
+
+                                            <div class="d-flex justify-content-between mt-3 mb-2">
+                                                <h3 class="text-light font-kh-bayon">${e.title}</h3>
+                                                <h3 class="text-blue-400 fw-bold">${e.year}</h3>
+                                            </div>
+                                            <div class="d-flex justify-content-between mt-3">
+                                                <p class="border-danger border-2 px-2 text-light fw-bold m-0 text-blue-400">HD</p>
+                                                <div class="pe-4 pe-md-2 pe-lg-0 d-flex align-items-center">
+                                                    <i class="bi bi-alarm me-2 fw-bolder text-light"></i>
+                                                    <p class="text-light m-0 fw-bolder">${e.duration}</p>
+                                                    &ensp;
+                                                    <i class="bi bi-star-fill me-2 fw-bolder text-blue-400 fw-bold"></i>
+                                                    <p class="text-light m-0 fw-bolder">${e.rate}</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                     </div>
+             `
+})
